1171a599f95e01e9b95afb3e42cb9629fff980e3
successbrian <brian.lathe@outlook.com>
2026-03-06 04:58:29 -0600

    Add ecosystemAuth and useUnifiedAuth to npm package

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,17 @@
  * Exported modules:
  * - utils/fingerprint - Browser fingerprinting
  * - utils/analytics - GA4 analytics
+ * - lib/ecosystemAuth - Unified authentication
+ * - hooks/useUnifiedAuth - React auth hook
  * 
  * Usage:
  *   import { fingerprint } from '@successbrian/global-assets'
- *   import { initAnalytics } from '@successbrian/global-assets'
+ *   import { ecosystemSignup } from '@successbrian/global-assets'
+ *   import { useUnifiedAuth } from '@successbrian/global-assets'
  */
 
 // Re-export all modules for convenient importing
 export * from './utils/fingerprint';
 export * from './utils/analytics';
+export * from './lib/ecosystemAuth';
+export { useUnifiedAuth, type UnifiedAuthState, type UseUnifiedAuthOptions } from './hooks/useUnifiedAuth';
